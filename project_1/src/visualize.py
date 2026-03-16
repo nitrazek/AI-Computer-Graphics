4284e54b5eb8fe61530ebf2f54b20c8749df13a9
Joker066 <alexdad063@gmail.com>
2026-03-16 20:16:39 +0100

    all models results

--- project_1/src/visualize.py
+++ project_1/src/visualize.py
@@ -13,15 +13,36 @@ def reconstruct_image(model_path, input_image_path):
     return reconstructed_tensor.astype('uint8')
 
 if __name__ == "__main__":
-    original_image = cv2.imread('../data/validation/clean/0900_0.png')
-    blurred_image = cv2.imread('../data/validation/blurred_3/0900_0.png')
+    print('Visualize denoising with sigma=0.01')
+    original_image = cv2.imread('../data/validation/clean/0801_0.png')
+    blurred_image = cv2.imread('../data/validation/noisy_001/0801_0.png')
+    reconstructed_image = reconstruct_image('../models/denoising_001_model.pth', '../data/validation/noisy_001/0801_0.png')
 
-    model = ImageRestorationCNN()
-    model.load_state_dict(torch.load('../models/denoising_model.pth', map_location=torch.device('cpu')))
+    cv2.imwrite('../visualizations/DN001_original_image.png', original_image) # type: ignore
+    cv2.imwrite('../visualizations/DN001_noisy_image.png', blurred_image) # type: ignore
+    cv2.imwrite('../visualizations/DN001_reconstructed_image.png', reconstructed_image)
 
-    reconstructed_image = reconstruct_image('../models/denoising_model.pth', '../data/validation/blurred_3/0900_0.png')
+    print('Visualize denoising with sigma=0.03')
+    original_image = cv2.imread('../data/validation/clean/0802_0.png')
+    blurred_image = cv2.imread('../data/validation/noisy_003/0802_0.png')
+    reconstructed_image = reconstruct_image('../models/denoising_003_model.pth', '../data/validation/noisy_003/0802_0.png')
 
-    cv2.imwrite('original_image.png', original_image) # type: ignore
-    cv2.imwrite('blurred_image.png', blurred_image) # type: ignore
-    cv2.imwrite('reconstructed_image.png', reconstructed_image)
+    cv2.imwrite('../visualizations/DN003_original_image.png', original_image) # type: ignore
+    cv2.imwrite('../visualizations/DN003_noisy_image.png', blurred_image) # type: ignore
+    cv2.imwrite('../visualizations/DN003_reconstructed_image.png', reconstructed_image)
 
+    print('Visualize deblurring with kernel size 3')
+    original_image = cv2.imread('../data/validation/clean/0803_0.png')
+    blurred_image = cv2.imread('../data/validation/blurred_3/0803_0.png')
+    reconstructed_image = reconstruct_image('../models/deblurring_3_model.pth', '../data/validation/blurred_3/0803_0.png')
+    cv2.imwrite('../visualizations/DB003_original_image.png', original_image) # type: ignore
+    cv2.imwrite('../visualizations/DB003_blurred_image.png', blurred_image) # type: ignore
+    cv2.imwrite('../visualizations/DB003_reconstructed_image.png', reconstructed_image)
+
+    print('Visualize deblurring with kernel size 5')
+    original_image = cv2.imread('../data/validation/clean/0804_0.png')
+    blurred_image = cv2.imread('../data/validation/blurred_5/0804_0.png')
+    reconstructed_image = reconstruct_image('../models/deblurring_5_model.pth', '../data/validation/blurred_5/0804_0.png')
+    cv2.imwrite('../visualizations/DB005_original_image.png', original_image) # type: ignore
+    cv2.imwrite('../visualizations/DB005_blurred_image.png', blurred_image) # type: ignore
+    cv2.imwrite('../visualizations/DB005_reconstructed_image.png', reconstructed_image)
